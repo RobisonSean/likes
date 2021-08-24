793acb33cf7c8ef8bbb4e9e9b4a949e0324fc570
RobisonSean <seanrobison013@gmail.com>
2021-08-24 15:24:11 -0700

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var count =9;
+
+function countUp(){
+    myElem = document.querySelector(".neilLike")
+    count++
+    console.log(count);
+    myElem.innerText = count;
+}
+
+var count1 =12;
+
+function countUp1(){
+    myElem = document.querySelector(".nicholeLike")
+    count1++
+    console.log(count1);
+    myElem.innerText = count1;
+}
+
+var count2 =90;
+
+function countUp2(){
+    myElem = document.querySelector(".jimLike")
+    count2++
+    console.log(count2);
+    myElem.innerText = count2;
+}
